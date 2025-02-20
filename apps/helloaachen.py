cf7dea6f7b080357d111015887d8dde5338d4ce5
Mohamed <moha-outma@hotmail.fr>
2025-02-20 12:50:17 +0100

    add a function for POST requests

--- apps/helloaachen.py
+++ apps/helloaachen.py
@@ -1,10 +1,12 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import socket, redis, json
+
+redis_client = redis.Redis(host='127.0.0.1', port=6379, db=0, decode_responses=True)
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def _send_response(self, message, status_code):
         client_ip = self.client_address[0]
-        server_ip = self.server.server_address[0]
-        full_message = f"{message}\n\nClient IP: {client_ip}\nServer IP: {server_ip}"
+        full_message = f"{message}\n\nClient IP: {client_ip}\n"
         self.send_response(status_code)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
@@ -17,7 +19,27 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self._send_response("Version : 1.0.0.1", 200)
         elif self.path == '/healthz':
             self._send_response("OK", 200)
+        elif self.path == '/messages':
+            messages = redis_client.lrange("key", 0, -1)
+            self._send_response(json.dumps(messages, indent=2), 200)
+
+    def do_POST(self):
+        if self.path == '/messages':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            try:
+                message = json.loads(post_data.decode('utf-8'))
+                redis_client.rpush("key", json.dumps(message))
+                self._send_response("Message stored successfully", 200)
+            except json.JSONDecodeError:
+                self._send_response("Invalid JSON", 400)
+        else:
+            self._send_response("Not Found", 404)
 
 httpd = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 print("Server started at http://localhost:8000")
 httpd.serve_forever()
+
+
+# Use CMD to test sending POST requests to the server using curl
+# curl -X POST http://localhost:8000/messages -H "Content-Type: application/json" -d "{\"message\": \"Hello, Aachen again!\"}"
