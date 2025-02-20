0723ede4cbc7187acad66aa09079bc67fc68346e
Mohamed <moha-outma@hotmail.fr>
2025-02-20 12:48:25 +0100

    update script

--- scripts/run_redis.sh
+++ scripts/run_redis.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-PASSWORD=$(cat /etc/redis-passwd/passwd)
+if [ -f /etc/redis-passwd/passwd ]; then
+    PASSWORD=$(cat /etc/redis-passwd/passwd)
     redis-server --requirepass $PASSWORD
+else
+    echo "Password file not found!"
+    exit 1
+fi
